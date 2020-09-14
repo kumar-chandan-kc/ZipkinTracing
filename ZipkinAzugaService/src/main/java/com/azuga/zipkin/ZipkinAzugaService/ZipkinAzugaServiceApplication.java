92244fee52288bfc5ca97ebfd21be813544d7fab
chandank <chandank@azuga.com>
2020-09-14 11:12:57 +0530

    Zipkin initial commit

---
+++ ZipkinAzugaService/src/main/java/com/azuga/zipkin/ZipkinAzugaService/ZipkinAzugaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.azuga.zipkin.ZipkinAzugaService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinAzugaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinAzugaServiceApplication.class, args);
+	}
+
+}
---
+++ ZipkinAzugaService/src/test/java/com/azuga/zipkin/ZipkinAzugaService/ZipkinAzugaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azuga.zipkin.ZipkinAzugaService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinAzugaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
